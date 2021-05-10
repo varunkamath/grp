c4c5d9c449c88c41b9df108af21a3cd70f3e8d74
varun <varun.kamath@gmail.com>
2021-05-10 16:25:57 -0500

    message fix

--- scripts/gesture-controller.py
+++ scripts/gesture-controller.py
@@ -14,7 +14,7 @@ def callback(gestures):
 
 
 # Steering
-    v.angular.z = -gestures.dir
+    v.angular.z = -(gestures.dir)
  #   if gestures.dir == "s":
   #      v.angular.z = 0
    # elif gestures.dir == "l":
