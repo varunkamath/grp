7494e29f9939492403fffd4eceabf5278a42f9f4
varun <varun.kamath@gmail.com>
2021-05-11 00:45:01 -0500

    nice

--- scripts/recognize.py
+++ scripts/recognize.py
@@ -137,7 +137,7 @@ if __name__ == "__main__":
     while not rospy.is_shutdown():
         #msg.fingers = 3
         pub.publish(msg)
-        r.sleep()
+        # r.sleep()
 
         # Note: msg.fingers is read only
         #print(msg.fingers)
